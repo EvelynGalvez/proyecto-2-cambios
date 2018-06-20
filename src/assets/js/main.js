9fbb82a1103ba57215f380ac38960c8d0adb621a
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-06-20 12:50:32 -0400

    primer commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,14 @@
+const btnCohort = document.getElementById('btn-cohort');
+const botonalumnas = document.getElementById('btn-alumnas');
+
+
+
+btnCohort.addEventListener('click', () => {
+  document.getElementById('navbar').style.display = 'block';
+  document.getElementById('divbtn-alumnas').style.display = 'block'; 
+  document.getElementById('btnCohort').style.display = 'none';
+})
+
+botonalumnas.addEventListener('click', () => {
+  document.getElementById('imgLaboratoria').style.display = 'none';
+})
